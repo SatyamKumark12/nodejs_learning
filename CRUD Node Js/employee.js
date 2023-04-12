e8759f31b1b7255ec8d43ec3498a48d4714a1cd3
Satyamhuge <satyam.huge@gmail.com>
2023-04-12 15:05:06 +0530

    basic crud in node js and postgres

---
+++ CRUD Node Js/employee.js
@@ -0,0 +1,106 @@
+const Pool = require("pg").Pool
+
+
+
+const pool = new Pool({
+    user : process.env.user,
+    host : 'localhost',
+    database : "postgres-api",
+    password : process.env.password,
+    port : 5432
+})
+
+// Post 
+
+const createEmployee = (req,res) =>{
+    const {name , email} = req.body
+
+
+    pool.query('INSERT INTO employees (name , email) VALUES ($1 , $2) RETURNING * ' , [name , email] , (err ,result)=>{
+        if(err){
+            console.log(err);
+            throw err;
+        }
+
+        res.status(200).json({
+            msg:"Data created successfully",
+            data:result.rows[0],
+        });
+    })
+
+}
+
+// Get All
+
+const getEmployees = (req , res) =>{
+    pool.query('SELECT * FROM employees' , (err , result) =>{
+        if(err){
+            throw err;
+        } 
+
+        res.json(
+            data = result.rows
+        )
+    })
+}
+
+ // Get Single Id
+
+
+const getEmployeeById = (req , res) =>{
+
+    let id = parseInt(req.params.id)
+
+
+    pool.query('SELECT * FROM employees WHERE id=$1' , [id] , (err , result) =>{
+        if(err){
+            throw err;
+        } 
+
+        res.json(
+            data = result.rows
+        )
+    })
+}
+
+ // Update 
+
+const updateEmployee = (req,res) =>{
+
+
+    let id = parseInt(req.params.id)
+    const { name , email } = req.body
+
+    pool.query('UPDATE employees SET name = $1 , email = $2 WHERE id = $3' , [name , email , id] , (err , result) => {
+        if(err){
+            throw err;
+        }
+
+        res.json({
+            data : "Updated Successfully"
+        })
+    })
+}
+
+ //  Delete
+
+const deleteEmployee = (req,res) =>{
+    
+    const id = parseInt(req.params.id)
+
+    pool.query('DELETE FROM employees WHERE id=$1',[id] , (err , result) => {
+        if(err){
+            throw err;
+        }
+
+        res.json({
+            msg : ` Employees with id ${id} Deleted successfully`
+        })
+    })
+}
+
+
+
+module.exports = {
+    createEmployee , getEmployees , getEmployeeById , updateEmployee , deleteEmployee
+}
